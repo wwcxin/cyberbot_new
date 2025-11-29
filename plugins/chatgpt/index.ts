b7046452916a160dda15a8448523928e7c27f7cd
wwcx <85999717+wwcxin@users.noreply.github.com>
2025-11-29 23:26:03 +0800

    Update index.ts
    新增以被引用消息当作用户prompt

--- plugins/chatgpt/index.ts
+++ plugins/chatgpt/index.ts
@@ -9,7 +9,7 @@ const config = {
         models: 'gpt-5-mini'
     },
     BOT: {
-        triggerWord: ".gpt ",
+        triggerWord: ".gpt",
         blockedGroups: [123456789, 87654321],
         systemPrompt: "请你扮演一个在 QQ 群里进行互动的全能问答机器人，你拥有海量的知识储备，可以极其熟练、正确的回答各种问题，你的回答生动而形象，回复内容中恰到好处地插入许多 emoji，得体而不胡哨，请你以 GitHub Flavored Markdown 格式回复，必要时可以使用高级语法，如 GitHub styled Alert 语法（比如 `> [!NOTE]  \nThis is tip.` 还有 TIP、IMPORTANT、WARNING、CAUTION 类型）、表格、Katex 数学公式等等，你尽可能的使用其强大的语法和生动形象的语言来呈现你的回答，每次回答都不少于 100 个字符，除非群友特殊说明，请尽可能使用中文回复。为了更加真实的扮演真实群友，请你不要介意'机器人回复不够礼貌'的问题，你可以在情节合适且必要时附带些嘲讽、挖苦、看热闹不嫌事大的语气。接下来请你回复或解答以下群友的问题，请直接回复下列内容："
     },
@@ -37,8 +37,9 @@ const plugin: Plugin = {
   handlers: {
     message: async (e) => {
         if (!ctx.getText(e).startsWith(config.BOT.triggerWord)) return;
-
-        const prompt = ctx.getText(e).replace(config.BOT.triggerWord, '').trim();
+        const quotedText = await ctx.getQuotedText(e)
+        if (ctx.getText(e).replace(config.BOT.triggerWord, '').trim() == '' && quotedText == '') return;
+        const prompt = quotedText || ctx.getText(e).replace(config.BOT.triggerWord, '').trim();
         const response = await client.chat.completions.create({
             model: config.openai.models,
             messages: [
